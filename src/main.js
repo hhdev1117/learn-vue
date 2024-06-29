08045ce7117017f0818b629f60b907680b4fd87a
harrison <hhdev1117@gmail.com>
2024-06-29 23:46:39 +0900

    프로젝트 생성

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import App from "./App.js";
+import AppHeader from "./components/AppHeader.js";
+import AppNav from "./components/AppNav.js";
+import AppView from "./components/AppView.js";
+import BookComponent from "./components/BookComponent.js";
+
+const app = Vue.createApp(App);
+app.component("AppHeader", AppHeader);
+app.component("AppNav", AppNav);
+app.component("AppView", AppView);
+app.component("BookComponent", BookComponent);
+
+app.mount("#app");
